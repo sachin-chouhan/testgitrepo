eef12b3b6e4ac67536b9b6303d59e9eccfaa6c4e
LaddU <LaddU@LADDU>
2017-11-22 00:04:44 +0530

    trsss

---
+++ testgit/testgit/Models/ErrorViewModel.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace testgit.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        public int MyProperty { get; set; }
+    }
+}
